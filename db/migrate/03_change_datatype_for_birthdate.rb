b0457b7fbd30f427e3323cb7a74a6c6f057c0e93
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-09 17:20:25 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,5 @@
-class ChangeDatatypeForBirthdat < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
+    change_column :student, :birthday, :datetime
   end
 end
